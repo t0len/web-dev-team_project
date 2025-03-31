99806a8d012b7396bbba3b512aa3c85436bb11dc
T0Len <leelogn3@gmail.com>
2025-03-31 22:40:27 +0500

    first commit

---
+++ src/app/components/book-list/book-list.component.ts
@@ -0,0 +1,26 @@
+import { Component, OnInit } from '@angular/core';
+import { BookService } from '../../services/book.service';
+import { Book } from '../../models/book.model';
+import { Router } from '@angular/router';
+
+@Component({
+  selector: 'app-book-list',
+  templateUrl: './book-list.component.html',
+  styleUrls: ['./book-list.component.scss']
+})
+export class BookListComponent implements OnInit {
+
+  books: Book[] = [];
+
+  constructor(private bookService: BookService, private router: Router) { }
+
+  ngOnInit(): void {
+    this.bookService.getBooks().subscribe((data: Book[]) => {
+      this.books = data;
+    });
+  }
+
+  viewBook(id: number): void {
+    this.router.navigate([`/book/${id}`]);
+  }
+}
